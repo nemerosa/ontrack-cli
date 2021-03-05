4e9857247c4edc2ca83bb8d3672122d38f92447c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-03-05 16:14:38 +0100

    Config list

---
+++ cmd/configList.go
@@ -0,0 +1,62 @@
+/*
+Copyright © 2021 Damien Coraboeuf <damien.coraboeuf@nemerosa.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	config "ontrack-cli/config"
+)
+
+// configListCmd represents the configList command
+var configListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all configurations",
+	Long:  `Displays the list of all existing configurations.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		root := config.ReadRootConfiguration()
+		for _, item := range root.Configurations {
+			if item.Name == root.Selected {
+				fmt.Printf("* %s\n", item.Name)
+			} else {
+				fmt.Printf("  %s\n", item.Name)
+			}
+			fmt.Printf("  %s\n", item.URL)
+		}
+	},
+}
+
+func init() {
+	configCmd.AddCommand(configListCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// configListCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// configListCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
